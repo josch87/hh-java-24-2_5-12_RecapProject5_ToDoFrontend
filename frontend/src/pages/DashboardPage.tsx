7d624633e76e4301cae2b867126dd26081df9d15
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-29 23:33:12 +0200

    Add task statistics to Dashboard page
    Added a task statistics section to the Dashboard page which fetches and displays data about the number and status of tasks. This was achieved by using axios to send a get request to "/api/todo", and then applying the useState and useEffect React hooks to manage and display the fetched data.

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,9 +1,55 @@
 import MainTemplate from "./templates/MainTemplate.tsx";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {TaskType} from "../model/model.ts";
 
 export default function DashboardPage() {
+
+    const [tasks, setTasks] = useState<TaskType[]>([]);
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    function getTasks() {
+        axios.get("/api/todo")
+            .then((response) => {
+                setTasks(response.data)
+            })
+    }
+
+
+
     return (
         <MainTemplate>
             <h1>Dashboard</h1>
+            <h2>Statistics</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Status</th>
+                    <th>Count</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Open</td>
+                    <td>{tasks.filter((task) => task.status === "OPEN").length}</td>
+                </tr>
+                <tr>
+                    <td>In Progress</td>
+                    <td>{tasks.filter((task) => task.status === "IN_PROGRESS").length}</td>
+                </tr>
+                <tr>
+                    <td>Done</td>
+                    <td>{tasks.filter((task) => task.status === "DONE").length}</td>
+                </tr>
+                <tr>
+                    <th>Total</th>
+                    <th>{tasks.length}</th>
+                </tr>
+                </tbody>
+            </table>
         </MainTemplate>
     )
 }
