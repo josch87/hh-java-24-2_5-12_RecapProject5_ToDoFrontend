fb013aab83181489ce1b3b51f1959291b3224042
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-29 17:06:42 +0200

    Add TaskDetailsPage and route
    A new TaskDetailsPage has been added to the application. This page allows viewing details of a specific task. A corresponding route in App.tsx is also added.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import TasksPage from "./pages/tasks/TasksPage.tsx";
 import CreateTaskPage from "./pages/tasks/CreateTaskPage.tsx";
+import TaskDetailsPage from "./pages/tasks/TaskDetailsPage.tsx";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/tasks" element={<TasksPage />} />
+            <Route path="/tasks/:id" element={<TaskDetailsPage />} />
             <Route path="/tasks/new" element={<CreateTaskPage />} />
         </Routes>
     )
---
+++ frontend/src/pages/tasks/TaskDetailsPage.tsx
@@ -0,0 +1,36 @@
+import MainTemplate from "../templates/MainTemplate.tsx";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {TaskType} from "../../model/model.ts";
+
+export default function TaskDetailsPage() {
+    const params = useParams();
+
+    const initialTask: TaskType = {
+        id: "",
+        description: "",
+        status: "OPEN"
+    }
+    const [task, setTask] = useState<TaskType>(initialTask);
+
+    useEffect(() => {
+        getTask();
+    }, []);
+
+    function getTask() {
+        axios.get("/api/todo/" + params.id)
+            .then((response) => {
+                setTask(response.data);
+            })
+            .catch((error) => {
+                console.error(error.message);
+            })
+    }
+
+    return (
+        <MainTemplate>
+            <h1>{task.description}</h1>
+        </MainTemplate>
+    )
+}
